c0bc893128d7cd6594ec43d53e255dc0c7728903
Shashikanth M S <90105859+shashikanth-m-s@users.noreply.github.com>
2021-11-28 00:31:10 +0530

    Create scaffold.dart

---
+++ scaffold.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
